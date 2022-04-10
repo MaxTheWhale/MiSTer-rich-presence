30feb81e70a8b4b7e84dc82585e74ab9e6b7adb1
Max Whale <maxwhale99@gmail.com>
2022-04-10 22:04:06 +0100

    Make update script self updating

--- server/scripts/discord_service_update.sh
+++ server/scripts/discord_service_update.sh
@@ -1,18 +1,22 @@
-RELEASE_URL=https://github.com/MaxTheWhale/MiSTer-rich-presence/releases/download/v0.1/mister_status_v0.1.zip
+RELEASE_URL=https://github.com/MaxTheWhale/MiSTer-rich-presence/releases/latest/download/server.zip
 OUTPUT_FILE=release.zip
+
 BIN_FILE=mister_status
 SVC_FILE=S92status
+SCRIPTS_DIR=scripts
+
+SCRIPTS_OUTPUT_DIR=/media/fat/Scripts
 
 echo "Fetching latest release..."
 curl -L -o $OUTPUT_FILE $RELEASE_URL
 
 echo "Extracting files..."
-unzip -o $OUTPUT_FILE $BIN_FILE $SVC_FILE
+unzip -o $OUTPUT_FILE
 chmod +x $BIN_FILE $SVC_FILE
-rm $OUTPUT_FILE
 
 echo "Copying files..."
 mv $BIN_FILE /sbin
+cp $SCRIPTS_DIR/*.sh $SCRIPTS_OUTPUT_DIR
 
 if [ -f /etc/init.d/$SVC_FILE ]; then
     mv $SVC_FILE /etc/init.d
@@ -20,6 +24,9 @@ if [ -f /etc/init.d/$SVC_FILE ]; then
 else 
     mv $SVC_FILE /etc/init.d/_$SVC_FILE
 fi
+
+rm $OUTPUT_FILE
+rm -r $SCRIPTS_DIR
 
 echo "Discord status service is installed and up-to-date."
 
